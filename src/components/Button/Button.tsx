9b219f1a3cfee91d2ca641ff53a09faf8748791d
Jonatan Mata <Jonatan.Mata@mdlz.com>
2023-08-17 20:32:03 -0600

    feat(ts): Adding FontAwsomeIcons
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 /**
  * Button properties.
- * 
+ *
  * @typedef {Object} ButtonProps
- * 
+ *
  * @property {string} label - Label to display on the button.
  * @property {('primary' | 'secondary')} [variant='primary'] - Button variant. Defaults to 'primary'.
  * @property {() => void} [onClick] - Click event handler for the button.
@@ -12,40 +14,45 @@ import React from 'react';
  */
 interface ButtonProps {
   label: string;
-  variant?: 'primary' | 'secondary'; // Allow two variants of the button
-  onClick?: () => void;  // Optional click handler
-  className?: string;    // Allow additional classes for more customization
+  variant?: "primary" | "secondary"; // Allow two variants of the button
+  onClick?: () => void; // Optional click handler
+  className?: string; // Allow additional classes for more customization
+  loading?: boolean;
 }
 
 /**
  * Button component.
- * 
+ *
  * @param {ButtonProps} props - Properties passed to the component.
- * 
+ *
  * @returns {React.FC<ButtonProps>} Button element.
  */
-const Button: React.FC<ButtonProps> = ({ 
-  label, 
-  variant = 'primary',
+const Button: React.FC<ButtonProps> = ({
+  label,
+  variant = "primary",
   onClick,
-  className
+  className,
+  loading,
 }) => {
   // Determine button style based on the variant
-  const baseStyle = 'py-2 px-4 rounded';
-  const primaryStyle = 'bg-blue-500 text-white hover:bg-blue-600';
-  const secondaryStyle = 'bg-gray-200 text-gray-800 hover:bg-gray-300';
-  
-  const style = variant === 'primary' ? primaryStyle : secondaryStyle;
+  const baseStyle = "py-2 px-4 rounded";
+  const primaryStyle = "bg-blue-500 text-white hover:bg-blue-600";
+  const secondaryStyle = "bg-gray-200 text-gray-800 hover:bg-gray-300";
+
+  const style = variant === "primary" ? primaryStyle : secondaryStyle;
 
   return (
-    <button 
-      className={`${baseStyle} ${style} ${className}`} 
-      onClick={onClick}
-    >
-      {label}
+    <button className={`${baseStyle} ${style} ${className}`} onClick={onClick}>
+      {loading ? (
+        <span className="flex items-center">
+          <FontAwesomeIcon icon={faCircleNotch} spin />
+          <span className="ml-2">Loading...</span>
+        </span>
+      ) : (
+        <span>{label}</span>
+      )}
     </button>
   );
-}
+};
 
 export default Button;
-
